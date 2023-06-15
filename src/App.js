82fe6a708ec654a3dc06e2ef142ced1f6489a4dc
Nick Sasseen <40394298+NickSasseen@users.noreply.github.com>
2023-06-15 03:13:54 +0000

    added initial app bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
+import { AppBar, Box, Container, IconButton, Toolbar, Typography } from '@mui/material';
 import './App.css';
+import { DarkMode, DeveloperMode, LightMode, Login } from '@mui/icons-material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import MenuIcon from "@mui/icons-material/Menu";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { useState, useMemo } from 'react';
+
+const pages = ["Movies"];
 
 function App() {
+  const [colorThemeIsDark, setColorThemeIsDark] = useState(useMediaQuery('(prefers-color-scheme: dark)'));
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: !colorThemeIsDark ? 'dark' : 'light',
+        },
+      }),
+    [colorThemeIsDark],
+  );
+
+  const themeIcon = colorThemeIsDark ? 
+      <DarkMode /> : <LightMode />
+
+  const changeColorTheme = () => setColorThemeIsDark(!colorThemeIsDark);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <DeveloperMode sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+
+            {
+              // Mobile 
+            }
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton size='large'>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+
+            <DeveloperMode sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              LOGO
+            </Typography>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <IconButton sx={{ p: 0 }} onClick={changeColorTheme}>
+                {
+                  themeIcon
+                }
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 }
 
