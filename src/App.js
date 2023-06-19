63dad61e839b27212259455999b442af25bd4397
Nick Sasseen <40394298+NickSasseen@users.noreply.github.com>
2023-06-19 01:39:06 +0000

    moved to separate component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import { AppBar, Box, Container, IconButton, Toolbar, Typography, Button, Menu, MenuItem, Drawer, List, ListItem, ListItemButton, ListItemText, ListItemIcon } from '@mui/material';
 import './App.css';
-import { DarkMode, DeveloperMode, LightMode, Login } from '@mui/icons-material';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import MenuIcon from "@mui/icons-material/Menu";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import React, { useState, useMemo } from 'react';
-
-const pages = ["Movies"];
+import AppBar from './components/layout/AppBar';
 
 function App() {
   const [colorThemeIsDark, setColorThemeIsDark] = useState(useMediaQuery('(prefers-color-scheme: dark)'));
-  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const [anchorElNav, setAnchorElNav] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   const theme = useMemo(
     () =>
@@ -34,127 +20,12 @@ function App() {
 
   const changeColorTheme = () => setColorThemeIsDark(!colorThemeIsDark);
 
-  const toggleDrawer = () => {
-    setDrawerIsOpen(!drawerIsOpen);
-  }
-
-  const lightDarkIcon =
-    <Box sx={{ flexGrow: 0 }}>
-      <IconButton sx={{ p: 0 }} onClick={changeColorTheme}>
-        {
-          colorThemeIsDark ? <DarkMode /> : <LightMode />
-        }
-      </IconButton>
-    </Box>
-
-  const mobileToolbar =
-    <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
-      <IconButton size='large' onClick={toggleDrawer}>
-        <MenuIcon />
-      </IconButton>
-      <DeveloperMode />
-      {
-        lightDarkIcon
-      }
-    </Toolbar>;
-
-  const desktopToolbar =
-    <Toolbar disableGutters>
-      <DeveloperMode />
-
-      <Box sx={{ flexGrow: 1, display: 'flex' }}>
-        {pages.map((page) => (
-          <Button
-            key={page}
-            onClick={handleCloseNavMenu}
-            sx={{ my: 2, color: 'white', display: 'block' }}
-          >
-            {page}
-          </Button>
-        ))}
-      </Box>
-
-      {
-        lightDarkIcon
-      }
-    </Toolbar>
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar>
-        <Container maxWidth="xl">
-          <Box sx={{display: {xs: 'block', md: 'none'}}}>
-            {
-              mobileToolbar
-            }
-          </Box>
-          <Box sx={{display: {xs: 'none', md: 'block'}}}>
-            {
-              desktopToolbar
-            }
-          </Box>
-
-          <Drawer
-            variant="temporary"
-            open={drawerIsOpen}
-            onClose={toggleDrawer}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-            sx={{ '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 250 } }}
-          >
-            <div>
-              <List>
-                {pages.map((text, index) => (
-                  <ListItem key={text} disablePadding>
-                    <ListItemButton>
-                      <ListItemIcon>
-                        {
-                          // PUT ICONS HERE
-                        }
-                      </ListItemIcon>
-                      <ListItemText primary={text} />
-                    </ListItemButton>
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-          </Drawer>
-        </Container>
-      </AppBar>
+      <AppBar changeColorTheme={changeColorTheme} themeIsDark={colorThemeIsDark} />
     </ThemeProvider>
   );
-
-  const otherToolbar = <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between' }}>
-    <DeveloperMode sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-      {pages.map((page) => (
-        <Button
-          key={page}
-          onClick={handleCloseNavMenu}
-          sx={{ my: 2, color: 'white', display: 'block' }}
-        >
-          {page}
-        </Button>
-      ))}
-    </Box>
-
-    {
-      // Mobile 
-    }
-    <Box sx={{ display: { xs: 'flex', md: 'none' }, justifyContent: "space-between" }}>
-      { /* menu icon */}
-      <Box>
-        <IconButton size='large' onClick={toggleDrawer}>
-          <MenuIcon />
-        </IconButton>
-      </Box>
-
-      { /* icon */}
-      <DeveloperMode />
-    </Box>
-  </Toolbar>
 }
 
 export default App;
---
+++ src/components/layout/AppBar.js
@@ -0,0 +1,100 @@
+import { AppBar as MuiAppBar, Box, Container, IconButton, Toolbar, Button, Drawer, List, ListItem, ListItemButton, ListItemText, ListItemIcon } from '@mui/material';
+import { DarkMode, DeveloperMode, LightMode } from '@mui/icons-material';
+import MenuIcon from "@mui/icons-material/Menu";
+import React, { useState } from 'react';
+
+const pages = ["Movies"];
+
+const AppBar = ({changeColorTheme, themeIsDark}) => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setDrawerIsOpen(!drawerIsOpen);
+      }
+
+    const lightDarkIcon =
+        <Box sx={{ flexGrow: 0 }}>
+            <IconButton sx={{ p: 0 }} onClick={() => changeColorTheme()}>
+                {
+                    themeIsDark ? <DarkMode /> : <LightMode />
+                }
+            </IconButton>
+        </Box>
+
+    const mobileToolbar =
+        <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
+            <IconButton size='large' onClick={toggleDrawer}>
+                <MenuIcon />
+            </IconButton>
+            <DeveloperMode />
+            {
+                lightDarkIcon
+            }
+        </Toolbar>;
+
+    const desktopToolbar =
+        <Toolbar disableGutters>
+            <DeveloperMode />
+
+            <Box sx={{ flexGrow: 1, display: 'flex' }}>
+                {pages.map((page) => (
+                    <Button
+                        key={page}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                        {page}
+                    </Button>
+                ))}
+            </Box>
+
+            {
+                lightDarkIcon
+            }
+        </Toolbar>
+
+    return (
+        <MuiAppBar>
+            <Container maxWidth="xl">
+                <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+                    {
+                        mobileToolbar
+                    }
+                </Box>
+                <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+                    {
+                        desktopToolbar
+                    }
+                </Box>
+
+                <Drawer
+                    variant="temporary"
+                    open={drawerIsOpen}
+                    onClose={toggleDrawer}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{ '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 250 } }}
+                >
+                    <div>
+                        <List>
+                            {pages.map((text, index) => (
+                                <ListItem key={text} disablePadding>
+                                    <ListItemButton>
+                                        <ListItemIcon>
+                                            {
+                                                // PUT ICONS HERE
+                                            }
+                                        </ListItemIcon>
+                                        <ListItemText primary={text} />
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </Drawer>
+            </Container>
+        </MuiAppBar>
+    );
+}
+
+export default AppBar;
